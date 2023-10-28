6c4aa7ee4ddae753ce0bdca666300fb7a70f8ed2
Tib <34336452+Tib-Gridello@users.noreply.github.com>
2023-10-28 12:32:11 -0400

    Update pi-ap-setup.sh

--- pi-ap-setup.sh
+++ pi-ap-setup.sh
@@ -2,8 +2,7 @@
 
 # Update and install required packages without prompts
 sudo DEBIAN_FRONTEND=noninteractive apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y hostapd dnsmasq nftables dhcpcd5 openvpn vim
-
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y hostapd dnsmasq nftables dhcpcd5 openvpn
 
 # Stop and disable the services while we configure them
 sudo systemctl stop hostapd
@@ -42,7 +41,6 @@ if [ ! -f "$DNSMASQ_CONF" ]; then
     echo "interface=wlan0
 dhcp-range=192.168.220.50,192.168.220.150,12h" | sudo tee "$DNSMASQ_CONF"
 fi
-
 # Set up IP forwarding with nftables
 NFTABLES_CONF="/etc/nftables.conf"
 if [ ! -f "$NFTABLES_CONF" ]; then
@@ -62,8 +60,14 @@ table ip filter {
     }
     chain output { type filter hook output priority 0; }
 }" | sudo tee "$NFTABLES_CONF"
+    
+    # Flush current ruleset
+    sudo nft flush ruleset
+    
+    # Load the new ruleset
     sudo nft -f "$NFTABLES_CONF"
 fi
+
 
 # Ensure IP Forwarding is enabled
 if grep -q '^#net.ipv4.ip_forward=1' /etc/sysctl.conf; then
@@ -77,15 +81,14 @@ sudo sysctl -p
 
 # Set static IP for wlan0
 if ! grep -q 'interface wlan0' /etc/dhcpcd.conf; then
-    echo "
-interface wlan0
-static ip_address=192.168.220.1/24" | sudo tee -a /etc/dhcpcd.conf
-    sudo systemctl restart dhcpcd
+    echo -e "\ninterface wlan0\nstatic ip_address=192.168.220.1/24" | sudo tee -a /etc/dhcpcd.conf
 fi
 
 # Enable and start the services
+sudo systemctl enable nftables
 sudo systemctl enable hostapd
 sudo systemctl enable dnsmasq
+sudo systemctl start nftables
 sudo systemctl start hostapd
 sudo systemctl start dnsmasq
 
