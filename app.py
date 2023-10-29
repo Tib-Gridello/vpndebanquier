d5438f3ff8ac8d5d5c4156e535ea674549d36f58
Tib-Gridello <>
2023-10-29 12:42:18 -0400

    wlan added

--- app.py
+++ app.py
@@ -6,12 +6,13 @@ import re
 import logging
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key_here'
+app.config['SECRET_KEY'] = 'your_secret_key_here'  # Used for form protection
 
 # Setup logging
 logging.basicConfig(filename='flask_app.log', level=logging.DEBUG)
 
 class WiFiForm(FlaskForm):
+    interface = SelectField('Interface', choices=[])
     ssid = SelectField('SSID', choices=[])
     password = PasswordField('Password')
 
@@ -19,22 +20,24 @@ class WiFiForm(FlaskForm):
 def index():
     form = WiFiForm()
 
+    # Whitelist interfaces
+    interfaces = [i for i in os.listdir('/sys/class/net/') if re.match(r'wlan\d+', i)]
+    form.interface.choices = [(i, i) for i in interfaces]
+
     if form.validate_on_submit():
+        interface = form.interface.data
         ssid = form.ssid.data
         password = form.password.data
-        # Here, add the logic to update the Wi-Fi connection
-        # Avoid direct command execution with user input
-        flash('Attempting to connect to {}'.format(ssid))
+        # Add the logic to update the Wi-Fi connection
+        flash('Attempting to connect to {} on {}'.format(ssid, interface))
         return redirect(url_for('index'))
 
-    # Set interface to wlan0 by default
-    selected_interface = 'wlan0'
+    # Set interface from form or default to wlan0
+    selected_interface = form.interface.data if form.interface.data else 'wlan0'
     command = ['sudo', 'iwlist', selected_interface, 'scan']
     scan_output = os.popen(' '.join(command)).read()
     ssids = [line for line in scan_output.splitlines() if "ESSID" in line]
-    logging.debug("Scan output for {}: {}".format(selected_interface, scan_output))
     ssids = [re.search(r'"(.*?)"', s).group(1) for s in ssids if re.search(r'"(.*?)"', s)]
-    logging.debug("Filtered SSIDs: {}".format(ssids))
     form.ssid.choices = [(s, s) for s in ssids]
     
     return render_template('index.html', form=form)
