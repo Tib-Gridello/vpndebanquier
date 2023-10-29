3e8342f33cf15341e51cc003db8ea6cd73b9ee44
Tib-Gridello <>
2023-10-29 12:46:42 -0400

    scan added

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_wtf import FlaskForm
-from wtforms import SelectField, PasswordField
+from wtforms import SelectField, PasswordField, SubmitField
 import os
 import re
 import logging
@@ -15,6 +15,8 @@ class WiFiForm(FlaskForm):
     interface = SelectField('Interface', choices=[])
     ssid = SelectField('SSID', choices=[])
     password = PasswordField('Password')
+    scan = SubmitField('Scan')
+    connect = SubmitField('Connect')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -25,22 +27,24 @@ def index():
     form.interface.choices = [(i, i) for i in interfaces]
 
     if form.validate_on_submit():
-        interface = form.interface.data
-        ssid = form.ssid.data
-        password = form.password.data
-        # Add the logic to update the Wi-Fi connection
-        flash('Attempting to connect to {} on {}'.format(ssid, interface))
-        return redirect(url_for('index'))
+        if form.scan.data:
+            selected_interface = form.interface.data
+            command = ['sudo', 'iwlist', selected_interface, 'scan']
+            scan_output = os.popen(' '.join(command)).read()
+            ssids = [line for line in scan_output.splitlines() if "ESSID" in line]
+            ssids = [re.search(r'"(.*?)"', s).group(1) for s in ssids if re.search(r'"(.*?)"', s)]
+            form.ssid.choices = [(s, s) for s in ssids]
+            return render_template('index.html', form=form, scanned=True)
 
-    # Set interface from form or default to wlan0
-    selected_interface = form.interface.data if form.interface.data else 'wlan0'
-    command = ['sudo', 'iwlist', selected_interface, 'scan']
-    scan_output = os.popen(' '.join(command)).read()
-    ssids = [line for line in scan_output.splitlines() if "ESSID" in line]
-    ssids = [re.search(r'"(.*?)"', s).group(1) for s in ssids if re.search(r'"(.*?)"', s)]
-    form.ssid.choices = [(s, s) for s in ssids]
-    
-    return render_template('index.html', form=form)
+        if form.connect.data:
+            # Add the logic to update the Wi-Fi connection
+            interface = form.interface.data
+            ssid = form.ssid.data
+            password = form.password.data
+            flash('Attempting to connect to {} on {}'.format(ssid, interface))
+            return redirect(url_for('index'))
+
+    return render_template('index.html', form=form, scanned=False)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5555)
