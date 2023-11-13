75e82bcb297113aa47342e86a0befa5510bb4b61
Tib <Tib>
2023-11-13 11:02:50 -0500

    sqd

--- app.py
+++ app.py
@@ -68,14 +68,14 @@ def execute_connection_script(internet_interface, hotspot_interface, ssid):
     wifi_creds_path = os.path.expanduser(f'home/naps/wifi/{ssid}')
     script_path = os.path.expanduser('~/vpndebanquier/wifi-wifi.sh')
     logging.debug(f"internet {internet_interface} hotspot {hotspot_interface} wifi {wifi_creds_path}")
-    command =   [
+    command = [
         script_path, 
-        '--internet=' , internet_interface, 
-        '--hotspot=' , hotspot_interface, 
-        '--wifi-creds=' , wifi_creds_path
+        '--internet', internet_interface, 
+        '--hotspot', hotspot_interface, 
+        '--wifi-creds', wifi_creds_path
     ]
     logging.debug(f"command = {command}")
-   # subprocess.run(command, check=True)
+    subprocess.run(command, check=True)
 
 @app.route('/', methods=['GET'])
 def index():
