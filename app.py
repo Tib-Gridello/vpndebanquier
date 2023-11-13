2401e0f6be6f8bdc0a0974a2fd419f24bbbe2718
Tib <Tib>
2023-11-13 13:04:51 -0500

    ezr

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def save_wifi_credentials(ssid, password):
 
 def execute_connection_script(internet_interface, hotspot_interface, ssid):
     logging.debug(f"Starting script with ssid {ssid}")
-    wifi_creds_path = os.path.expanduser(f'home/naps/wifi/{ssid}')
+    wifi_creds_path = os.path.expanduser(f'/home/naps/wifi/{ssid}')
     script_path = os.path.expanduser('~/vpndebanquier/wifi-wifi.sh')
     logging.debug(f"internet {internet_interface} hotspot {hotspot_interface} wifi {wifi_creds_path}")
     command = [
